1e588bd3549cae969f96a71a5ff6de52d5c302f5
emirue <emirue@naver.com>
2020-02-21 18:08:02 +0900

    client main typescript로 변경...

--- src/webapp/client/main.tsx
+++ src/webapp/client/main.tsx
@@ -1,6 +1,11 @@
-import React from 'react';
+/**
+ * 제작 : emirue
+ * 날짜 : 2020/02/21
+ * 내용 :
+ */
+import * as React from 'react';
 import { Meteor } from 'meteor/meteor';
-import ReactDOM from 'react-dom';
+import * as ReactDOM from 'react-dom';
 import App from '../imports/ui/App';
 import '../imports/startup/client/index';
 import '../imports/startup/both/index';
